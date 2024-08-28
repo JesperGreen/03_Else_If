6d1dfbef6d5f15ca15635b80083246e3058fd295
JesperGreen <jesper_gs_@hotmail.com>
2024-08-28 18:24:56 +0200

    Update Test_Score.cs

--- Test_Score/Test_Score.cs
+++ Test_Score/Test_Score.cs
@@ -1,1 +1,30 @@
+// Test Score
 
+class Program
+{ 
+    static void Main(string[] args) 
+    {
+        Console.Write("Input your test score: ");
+        int score = Convert.ToInt32(Console.ReadLine());
+        if (score < 60)
+        {
+            Console.WriteLine("F");
+        }
+        else if ((score >= 60) && (score < 69))
+        {
+             Console.WriteLine("D");
+        } 
+        else if ((score >= 70) && (score < 80))
+            {
+             Console.WriteLine("C");
+        } 
+        else if ((score >= 80) && (score < 90))
+        {
+             Console.WriteLine("B");
+        }
+        else if ((score >=90) && (score < 101))
+        {  Console.WriteLine("A");}
+
+        Console.ReadLine();
+    }
+}
