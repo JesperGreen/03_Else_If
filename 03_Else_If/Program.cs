078311635c435d404e574e1ae2be66d2e6b142e2
JesperGreen <jesper_gs_@hotmail.com>
2024-08-28 14:48:03 +0200

    Even or Odd

---
+++ 03_Else_If/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1. Even or Odd
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("Number is even");
+        }
+        else
+        {
+            Console.WriteLine("Number is odd");
+
+
+            Console.ReadLine();
+        }
+    }
+}
