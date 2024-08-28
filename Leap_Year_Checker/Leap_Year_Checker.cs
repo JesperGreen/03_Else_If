a7e6f2c68b72debc7bd8236f54e9c7ea9d3d249b
JesperGreen <jesper_gs_@hotmail.com>
2024-08-28 19:04:39 +0200

    Update Leap_Year_Checker.cs
    Leap Year Checker

--- Leap_Year_Checker/Leap_Year_Checker.cs
+++ Leap_Year_Checker/Leap_Year_Checker.cs
@@ -1,1 +1,20 @@
+// Leap Year Checker
 
+using System;
+public class Demo
+{
+    public static void Main()
+    {
+        Console.Write("input a year (4 digits) to see if it is a leap year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Year = " + year);
+        if (DateTime.IsLeapYear(year))
+        {
+            Console.WriteLine("This is a leap year");
+        }
+        else
+        {
+            Console.WriteLine("This is NOT a leap year");
+        }
+    }
+}
